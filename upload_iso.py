d0abd5aa230fd4fc097731845fac27204ca90929
Andrew <kezin636@gmail.com>
2025-01-14 00:37:55 +0300

    oh shoot

--- upload_iso.py
+++ upload_iso.py
@@ -1,12 +1,25 @@
 import os
 import subprocess
+import requests
+
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 
 for filename in os.listdir('out/'):
     print(f'Uploading {filename} to temp.sh ...')
 
-    process = subprocess.run([
-        'curl', '-F', f'file=@out/{filename}', 'https://temp.sh/upload'
-    ])
+    files = {
+        'file': open(filename, 'rb'),
+    }
 
-    print(f'\n\nUploaded: {filename}')
+    response = requests.post('https://temp.sh/upload', files=files)
+    final_url = response.text
+
+    print(f'\n\nUploaded: {filename} to {final_url}')
+
+    r = requests.post(f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage', json={
+        'chat_id': TELEGRAM_CHAT_ID,
+        'text': f'Uploaded {filename} to {final_url}, it\'s temporary'
+    })
