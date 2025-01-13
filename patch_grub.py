1405f2e400dc8370a0c4aa67843d6ba956f37467
Andrew <kezin636@gmail.com>
2025-01-14 00:28:23 +0300

    boom

--- patch_grub.py
+++ patch_grub.py
@@ -2,9 +2,9 @@ with open('archlive/grub/grub.cfg', 'r') as fp:
     grub_cfg = fp.read()
 
 
-grub_cfg.replace('play 600 988 1 1319 4', '')
-grub_cfg.replace('Arch Linux', 'Project GDL')
-grub_cfg.replace('gfxmode="auto"', 'gfxmode="1920x1080"')
+grub_cfg = grub_cfg.replace('play 600 988 1 1319 4', '')
+grub_cfg = grub_cfg.replace('Arch Linux', 'Project GDL')
+grub_cfg = grub_cfg.replace('gfxmode="auto"', 'gfxmode="1920x1080"')
 
 
 with open('archlive/grub/grub.cfg', 'w') as fp:
