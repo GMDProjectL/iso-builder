8a754f2808a4ce78a5dc6d6871447b0c3b18b1b0
Andrew <kezin636@gmail.com>
2025-01-14 16:59:55 +0300

    izhzhhz

--- patch_sdboot.py
+++ patch_sdboot.py
@@ -5,6 +5,18 @@ with open('archlive/efiboot/loader/entries/01-archiso-x86_64-linux.conf', 'r') a
 
 sd_archiso = sd_archiso.replace('Arch Linux', 'Project GDL')
 
+with open('archlive/efiboot/loader/entries/01-archiso-x86_64-linux.conf', 'w') as fp:
+    fp.write(sd_archiso)
+
+
+with open('archlive/efiboot/loader/entries/02-archiso-x86_64-speech-linux.conf', 'r') as fp:
+    sd2_archiso = fp.read()
+
+sd2_archiso = sd2_archiso.replace('Arch Linux', 'Project GDL')
+
+with open('archlive/efiboot/loader/entries/01-archiso-x86_64-speech-linux.conf', 'w') as fp:
+    fp.write(sd2_archiso)
+
 
 
 with open('archlive/efiboot/loader/loader.conf', 'r') as fp:
@@ -12,14 +24,5 @@ with open('archlive/efiboot/loader/loader.conf', 'r') as fp:
 
 sd_loader = sd_loader.replace('beep on', '')
 
-
-#os.remove('archlive/efiboot/loader/entries/02-archiso-x86_64-speech-linux.conf')
-#os.remove('archlive/efiboot/loader/entries/03-archiso-x86_64-memtest86+.conf')
-
-
-with open('archlive/efiboot/loader/entries/01-archiso-x86_64-linux.conf', 'w') as fp:
-    fp.write(sd_archiso)
-
 with open('archlive/efiboot/loader/loader.conf', 'w') as fp:
     fp.write(sd_loader)
-
