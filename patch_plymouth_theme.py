ce1df22f970126f08297846e534dfe1fda95d9cd
Andrew <kezin636@gmail.com>
2025-01-17 21:10:30 +0300

    stupid

--- patch_plymouth_theme.py
+++ patch_plymouth_theme.py
@@ -5,22 +5,9 @@ pmt_directory = 'archlive/airootfs/etc/plymouth'
 if not os.path.exists(pmt_directory):
     os.makedirs(pmt_directory)
 
-pmt_path = 'archlive/airootfs/etc/mkinitcpio.conf.d/archiso.conf'
-
-if not os.path.exists(pmt_path):
-    with open(pmt_path, 'w') as f:
-        f.write('[Daemon]\nTheme=michigun')
-        print('Created plymouth config from scratch!')
-        exit()
-
-with open(pmt_path, 'r') as f:
-    pmt = f.read()
-
-pmt = pmt.replace('#[Daemon]', '[Daemon]')
-pmt = pmt.replace('#Theme=fade-in', 'Theme=michigun')
+pmt_path = pmt_directory + '/plymouthd.conf'
 
 
 with open(pmt_path, 'w') as f:
-    f.write(pmt)
-
-print('Patched plymouth config!')
+    f.write('[Daemon]\nTheme=michigun')
+    print('Created plymouth config from scratch!')
